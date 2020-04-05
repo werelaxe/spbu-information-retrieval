708da38be8f03e0e82da87e71d9583d5e0595571
Daniil Sharko <daniil.sharko@jetbrains.com>
2020-04-05 22:11:58 +0300

    Add corpus fetcher

---
+++ src/corpus-generator/fetch.py
@@ -0,0 +1,27 @@
+import os
+import urllib.request
+import zipfile
+
+CORPUS_URL = "http://werelaxe.site:1234/corpus.zip"
+CORPUS_ZIP_FILENAME = "corpus.zip"
+CORPUS_DIRECTORY = "corpus"
+
+def main():
+    print("Start fetching corpus")
+    if not os.path.exists(CORPUS_ZIP_FILENAME):
+        response = urllib.request.urlopen(CORPUS_URL)
+        with open(CORPUS_ZIP_FILENAME, "wb") as corpus_file:
+            corpus_file.write(response.read())
+        print("corpus.zip has been downloaded")
+    else:
+        print("corpus.zip is already exists")
+    if not os.path.exists(CORPUS_DIRECTORY):
+        with zipfile.ZipFile(CORPUS_ZIP_FILENAME, 'r') as zip_ref:
+            zip_ref.extractall(".")
+        print("Corpus has been extracted")
+    else:
+        print("corpus directory is already exists")
+
+
+if __name__ == '__main__':
+    main()
